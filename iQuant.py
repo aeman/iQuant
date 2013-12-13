4b265726f885a767f3c634ff8c9ad05f3531ef59
aeman <aeman001@163.com>
2013-12-14 00:44:24 +0800

    Signed-off-by: aeman <aeman001@163.com>

--- iQuant.py
+++ iQuant.py
@@ -4,11 +4,33 @@
 import os,sys
 
 DATA_PATH = './data/'
+AVE_DAYS = 60
 
 #if sys.argv[1] == "-e":
-		
-f = file(DATA_PATH + 'test.TXT')
-for line in f.readlines():
-		value = line.split('\t')
-		print value[0], value[1]
-f.close()
+
+def load_data(data_file, data_list):
+	try:
+		f = file(data_file)
+		step = 0
+		for line in f.readlines():
+			step = step + 1
+			if step < 3: continue	#skip title
+			data_list.append(line)
+		f.close();
+	except:
+		return 0
+
+
+global m_list, s_list
+m_list = []; s_list = []
+load_data(DATA_PATH + 'test.TXT', m_list)
+load_data(DATA_PATH + 'SZ002142.TXT', s_list)
+
+yesterday = 0.0
+today = 0.0
+
+for one_day in m_list:
+	value = one_day.split('\t')
+	print value[0], value[4]
+	
+print len(m_list)
