9bf24a1d827791031f7afd260ed9a48910b99f22
aeman <aeman001@163.com>
2013-12-27 13:37:11 +0800

    iQuant3.py modify

--- iQuant3.py
+++ iQuant3.py
@@ -9,8 +9,7 @@ INIT_MONEY = 100000.0
 FIRST_MONEY = INIT_MONEY * 10000 / 223696
 DEAL_YEAR = 2008
 
-global s_list, hprice, d_price, times, all_money, all_count, s_count, in_stock, log
-s_list = []; times = 0; all_money = INIT_MONEY; all_count = 0; in_stock = False;
+
 
 def load_stock(data_file, data_list):
 	try:
@@ -59,36 +58,43 @@ def sell(price, deal_date):
 	all_count = 0
 	times = 0
 	in_stock = False
-	hprice = hprice * 0.8
+	hprice = hprice * 0.9
 	d_price = {1:hprice, 2:hprice*0.9, 3:hprice*0.9**2, 4:hprice*0.9**3, 5:hprice*0.9**4}
 	print hprice, d_price
 
-load_stock(DATA_PATH + 'SH600036.TXT', s_list)
-log = open(DATA_PATH + 'deal.log', 'w')
+def main():
+	global s_list, hprice, d_price, times, all_money, all_count, s_count, in_stock, log
+	s_list = []; times = 0; all_money = INIT_MONEY; all_count = 0; in_stock = False;
+	
+	load_stock(DATA_PATH + 'SH600036.TXT', s_list)
+	log = open(DATA_PATH + 'deal.log', 'w')
 
-hprice = get_hprice(s_list, DEAL_YEAR - 1); print hprice
-d_price = {1:hprice, 2:hprice*0.9, 3:hprice*0.9**2, 4:hprice*0.9**3, 5:hprice*0.9**4}
-d_ratio = {1:0.045, 2:0.08, 3:0.145, 4:0.261, 5:0.469}
+	hprice = get_hprice(s_list, DEAL_YEAR - 1); print hprice
+	d_price = {1:hprice, 2:hprice*0.9, 3:hprice*0.9**2, 4:hprice*0.9**3, 5:hprice*0.9**4}
+	d_ratio = {1:0.045, 2:0.08, 3:0.145, 4:0.261, 5:0.469}
 
-for s in s_list:
-	if string.atoi(s['rq'][:4]) < DEAL_YEAR: continue	#ignore 2007 data
-	print s['rq'], s['zg'], s['zd'], times, in_stock
-	if in_stock:
-		if times == 1:
-			if ((s['zd'] < d_price[1] * 1.05) and (s['zg'] > d_price[1] * 1.05)):
-				sell(d_price[1] * 1.05, s['rq']); continue
+	for s in s_list:
+		if string.atoi(s['rq'][:4]) < DEAL_YEAR: continue	#ignore 2007 data
+		print s['rq'], s['zg'], s['zd'], times, in_stock
+		if in_stock:
+			if times == 1:
+				if ((s['zd'] < d_price[1] * 1.05) and (s['zg'] > d_price[1] * 1.05)):
+					sell(d_price[1] * 1.05, s['rq']); continue
+			else:
+				if ((s['zd'] < d_price[1]) and (s['zg'] > d_price[1])):
+					sell(d_price[1], s['rq']); continue
+			if times == 5: continue
+			else:
+				if (s['zd'] < d_price[times+1]) and (s['zg'] > d_price[times + 1]):
+					buy(d_price[times + 1], s['rq'])
 		else:
-			if ((s['zd'] < d_price[1]) and (s['zg'] > d_price[1])):
-				sell(d_price[1], s['rq']); continue
-		if times == 5: continue
-		else:
-			if (s['zd'] < d_price[times+1]) and (s['zg'] > d_price[times + 1]):
-				buy(d_price[times + 1], s['rq'])
-	else:
-		if (s['zd'] < d_price[1]) and (s['zg'] > d_price[1]):
-			first_buy(FIRST_MONEY, d_price[1], s['rq'])
+			if (s['zd'] < d_price[1]) and (s['zg'] > d_price[1]):
+				first_buy(FIRST_MONEY, d_price[1], s['rq'])
+	
+	log.write('last_date:\tall_money%.2f\tall_count:%d\n' % (all_money, all_count))
+	log.close()
+	
+	print 'iQuant OK!'
 
-log.write('last_date:\tall_money%.2f\tall_count:%d\n' % (all_money, all_count))
-log.close()
-
-print 'iQuant OK!'
+if __name__ == '__main__':
+	sys.exit(main())
